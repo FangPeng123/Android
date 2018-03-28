b6cb8624125faa809e35c4a435d97de5585ce50c
Fang Peng <fapeng@microsoft.com>
2018-03-28 16:18:39 +0800

    tt

---
+++ Android/VSACFangAndroid1/VSACFangAndroid1/app/src/main/java/com/example/fapeng/vsacfangandroid1/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.fapeng.vsacfangandroid1;
+
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        AppCenter.start(getApplication(), "7bd14748-14c0-4272-b7c6-0fb98aa2460f",
+                Analytics.class, Crashes.class);
+        final Button button=findViewById(R.id.testbutton);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view)
+            {
+
+                Map<String, String> properties = new HashMap<>();
+                properties.put("Category", "Music");
+                properties.put("FileName", "favorite.avi");
+                Analytics.trackEvent("test",properties);
+                Analytics.trackEvent("Video clicked", properties);
+
+              int a=1;
+              int b=0;
+                int c=a/b;
+
+
+            }
+
+        });
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
